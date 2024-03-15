607f3a4920d91a17bbf6cacafa6ced7a69560a1c
joynalabedin05 <joynal05101993@gmail.com>
2024-03-15 23:02:16 +0600

    added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Next.Js-Authentication-Starter-Pack
+# expolre Next Auth
+## Installation:
+l2-f-m-26-nextjs-authentication-client
+1. Clone the repository.
+2. Install dependencies using `npm install`.
+3. Run the project using `npm run dev`.
---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,29 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
+  return (
+    <div>
+      {
+        session?.user && 
+        <>
+          <h1 className="text-4xl text-center mt-10">Welcome To: {session?.user?.name}</h1>
+          <h1 className="text-4xl text-center mt-10">Loggedin User Email: {session?.user?.email}</h1>
+          <Image
+            src={session?.user?.image ||
+              "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"}
+            width={200}
+            height={200}
+            alt="user image"
+            className="mx-auto mt-5 rounded-full"
+          />
+        </>
+      }
+    </div>
+  );
+};
+
+export default DashboardPage;
